ed32ce8837e61da9c461e0f3fd9ecb5f8cae3090
Jenelle Truong <jftruong@ucsd.edu>
2022-02-24 11:20:17 -0800

    crated libjpeg-turbo3 for Unix compilation and removed some UNSAFE_unverified()

---
+++ tensorflow/core/lib/jpeg/libjpeg-turbo3/libjpeg-turbo/CMakeFiles/turbojpeg.dir/DependInfo.cmake
@@ -0,0 +1,84 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jaricom.c" "CMakeFiles/turbojpeg.dir/jaricom.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jaricom.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcapimin.c" "CMakeFiles/turbojpeg.dir/jcapimin.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcapimin.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcapistd.c" "CMakeFiles/turbojpeg.dir/jcapistd.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcapistd.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcarith.c" "CMakeFiles/turbojpeg.dir/jcarith.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcarith.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jccoefct.c" "CMakeFiles/turbojpeg.dir/jccoefct.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jccoefct.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jccolor.c" "CMakeFiles/turbojpeg.dir/jccolor.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jccolor.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcdctmgr.c" "CMakeFiles/turbojpeg.dir/jcdctmgr.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcdctmgr.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jchuff.c" "CMakeFiles/turbojpeg.dir/jchuff.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jchuff.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcicc.c" "CMakeFiles/turbojpeg.dir/jcicc.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcicc.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcinit.c" "CMakeFiles/turbojpeg.dir/jcinit.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcinit.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcmainct.c" "CMakeFiles/turbojpeg.dir/jcmainct.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcmainct.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcmarker.c" "CMakeFiles/turbojpeg.dir/jcmarker.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcmarker.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcmaster.c" "CMakeFiles/turbojpeg.dir/jcmaster.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcmaster.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcomapi.c" "CMakeFiles/turbojpeg.dir/jcomapi.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcomapi.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcparam.c" "CMakeFiles/turbojpeg.dir/jcparam.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcparam.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcphuff.c" "CMakeFiles/turbojpeg.dir/jcphuff.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcphuff.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcprepct.c" "CMakeFiles/turbojpeg.dir/jcprepct.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcprepct.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jcsample.c" "CMakeFiles/turbojpeg.dir/jcsample.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jcsample.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jctrans.c" "CMakeFiles/turbojpeg.dir/jctrans.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jctrans.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdapimin.c" "CMakeFiles/turbojpeg.dir/jdapimin.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdapimin.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdapistd.c" "CMakeFiles/turbojpeg.dir/jdapistd.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdapistd.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdarith.c" "CMakeFiles/turbojpeg.dir/jdarith.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdarith.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdatadst-tj.c" "CMakeFiles/turbojpeg.dir/jdatadst-tj.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdatadst-tj.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdatadst.c" "CMakeFiles/turbojpeg.dir/jdatadst.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdatadst.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdatasrc-tj.c" "CMakeFiles/turbojpeg.dir/jdatasrc-tj.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdatasrc-tj.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdatasrc.c" "CMakeFiles/turbojpeg.dir/jdatasrc.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdatasrc.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdcoefct.c" "CMakeFiles/turbojpeg.dir/jdcoefct.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdcoefct.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdcolor.c" "CMakeFiles/turbojpeg.dir/jdcolor.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdcolor.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jddctmgr.c" "CMakeFiles/turbojpeg.dir/jddctmgr.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jddctmgr.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdhuff.c" "CMakeFiles/turbojpeg.dir/jdhuff.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdhuff.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdicc.c" "CMakeFiles/turbojpeg.dir/jdicc.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdicc.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdinput.c" "CMakeFiles/turbojpeg.dir/jdinput.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdinput.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdmainct.c" "CMakeFiles/turbojpeg.dir/jdmainct.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdmainct.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdmarker.c" "CMakeFiles/turbojpeg.dir/jdmarker.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdmarker.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdmaster.c" "CMakeFiles/turbojpeg.dir/jdmaster.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdmaster.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdmerge.c" "CMakeFiles/turbojpeg.dir/jdmerge.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdmerge.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdphuff.c" "CMakeFiles/turbojpeg.dir/jdphuff.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdphuff.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdpostct.c" "CMakeFiles/turbojpeg.dir/jdpostct.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdpostct.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdsample.c" "CMakeFiles/turbojpeg.dir/jdsample.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdsample.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jdtrans.c" "CMakeFiles/turbojpeg.dir/jdtrans.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jdtrans.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jerror.c" "CMakeFiles/turbojpeg.dir/jerror.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jerror.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jfdctflt.c" "CMakeFiles/turbojpeg.dir/jfdctflt.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jfdctflt.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jfdctfst.c" "CMakeFiles/turbojpeg.dir/jfdctfst.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jfdctfst.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jfdctint.c" "CMakeFiles/turbojpeg.dir/jfdctint.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jfdctint.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jidctflt.c" "CMakeFiles/turbojpeg.dir/jidctflt.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jidctflt.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jidctfst.c" "CMakeFiles/turbojpeg.dir/jidctfst.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jidctfst.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jidctint.c" "CMakeFiles/turbojpeg.dir/jidctint.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jidctint.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jidctred.c" "CMakeFiles/turbojpeg.dir/jidctred.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jidctred.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jmemmgr.c" "CMakeFiles/turbojpeg.dir/jmemmgr.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jmemmgr.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jmemnobs.c" "CMakeFiles/turbojpeg.dir/jmemnobs.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jmemnobs.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jquant1.c" "CMakeFiles/turbojpeg.dir/jquant1.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jquant1.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jquant2.c" "CMakeFiles/turbojpeg.dir/jquant2.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jquant2.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/jutils.c" "CMakeFiles/turbojpeg.dir/jutils.c.o" "gcc" "CMakeFiles/turbojpeg.dir/jutils.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/rdbmp.c" "CMakeFiles/turbojpeg.dir/rdbmp.c.o" "gcc" "CMakeFiles/turbojpeg.dir/rdbmp.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/rdppm.c" "CMakeFiles/turbojpeg.dir/rdppm.c.o" "gcc" "CMakeFiles/turbojpeg.dir/rdppm.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/transupp.c" "CMakeFiles/turbojpeg.dir/transupp.c.o" "gcc" "CMakeFiles/turbojpeg.dir/transupp.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/turbojpeg.c" "CMakeFiles/turbojpeg.dir/turbojpeg.c.o" "gcc" "CMakeFiles/turbojpeg.dir/turbojpeg.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/wrbmp.c" "CMakeFiles/turbojpeg.dir/wrbmp.c.o" "gcc" "CMakeFiles/turbojpeg.dir/wrbmp.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/wrppm.c" "CMakeFiles/turbojpeg.dir/wrppm.c.o" "gcc" "CMakeFiles/turbojpeg.dir/wrppm.c.o.d"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/libjpeg-turbo/libturbojpeg.0.dylib" "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/libjpeg-turbo/libturbojpeg.0.2.0.dylib"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/libjpeg-turbo/libturbojpeg.dylib" "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/libjpeg-turbo/libturbojpeg.0.2.0.dylib"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tensorflow/core/lib/jpeg/libjpeg-turbo3/libjpeg-turbo/sharedlib/CMakeFiles/cjpeg.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/cdjpeg.c" "sharedlib/CMakeFiles/cjpeg.dir/__/cdjpeg.c.o" "gcc" "sharedlib/CMakeFiles/cjpeg.dir/__/cdjpeg.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/cjpeg.c" "sharedlib/CMakeFiles/cjpeg.dir/__/cjpeg.c.o" "gcc" "sharedlib/CMakeFiles/cjpeg.dir/__/cjpeg.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/rdbmp.c" "sharedlib/CMakeFiles/cjpeg.dir/__/rdbmp.c.o" "gcc" "sharedlib/CMakeFiles/cjpeg.dir/__/rdbmp.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/rdgif.c" "sharedlib/CMakeFiles/cjpeg.dir/__/rdgif.c.o" "gcc" "sharedlib/CMakeFiles/cjpeg.dir/__/rdgif.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/rdppm.c" "sharedlib/CMakeFiles/cjpeg.dir/__/rdppm.c.o" "gcc" "sharedlib/CMakeFiles/cjpeg.dir/__/rdppm.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/rdswitch.c" "sharedlib/CMakeFiles/cjpeg.dir/__/rdswitch.c.o" "gcc" "sharedlib/CMakeFiles/cjpeg.dir/__/rdswitch.c.o.d"
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/rdtarga.c" "sharedlib/CMakeFiles/cjpeg.dir/__/rdtarga.c.o" "gcc" "sharedlib/CMakeFiles/cjpeg.dir/__/rdtarga.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/michaelgranado/tensorflow/rlbox_sandboxing_api/libjpeg-turbo/sharedlib/CMakeFiles/jpeg.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
